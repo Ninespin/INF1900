6dacc6bba5496dbc2f9689defaf985a41f681cc5
Jeremi <guitprodje@hotmail.ca>
2019-01-10 11:35:24 -0500

    V1 'interrupt'

--- TP1/compteur32bits.cpp
+++ TP1/compteur32bits.cpp
@@ -1,18 +1,11 @@
-/*
- * Nom: compteur 32 bits
- * Copyright (C) 2005 Matthew Khouzam
- * License http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * Description: Ceci est un exemple simple de programme 
- * Version: 1.1
- */
-
 #define F_CPU 8000000UL
 #include <avr/io.h> 
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <stdint.h>
 
 
+
+// CONTROLE DEL
 void setColorDelay(uint8_t color, uint16_t ms){
 	uint8_t currentColor = color;
 	uint8_t offset = 0;
@@ -29,10 +22,9 @@ void setColorDelay(uint8_t color, uint16_t ms){
 	}
 }
 
-int main()
+int mainControleDEL()
 {
   DDRB = 0xff; // PORT B est en mode sortie
-  uint8_t compteur = 0;
 
   for(;;)  // boucle sans fin
   {
@@ -42,4 +34,48 @@ int main()
   }
   return 0; 
 }
+
+
+
+int mainInterrupt(){
+	DDRD = 0x00;
+	DDRB = 0xFF;
+
+	bool lastBtn = 0;
+	bool btn = 0;
+	while(1){
+		btn = PIND & 0x04;
+
+		if(btn){
+			if(PORTB & 0x01){
+				PORTB = (PORTB & 0xFC) | 0x02;
+			}else if(PORTB & 0x02){
+				PORTB = (PORTB & 0xFC) | 0x01;
+			}else{
+				PORTB = (PORTB & 0xFC) | 0x01;
+			}
+		}
+
+		if(lastBtn && !btn){		
+			PORTB = 0x00;
+		}
+
+		lastBtn = btn;
+	}
+	
+	return 0;
+}
+
+
+
+
+int main(){
+	return mainInterrupt();
+
+}
+
+
+
+
+
 
