b895e64bca57b335af7183736291178f5cc71143
Jeremi <guitprodje@hotmail.ca>
2019-01-15 09:38:47 -0500

    makefile

--- TP2/pb1.cpp
+++ TP2/pb1.cpp
@@ -6,8 +6,83 @@
 
 
 
-int main(){
+//INT BTNPRESS
+
+
+class Btn{
+public:
+	Btn()
+		: state(false)
+		, lastState(false)
+		, debounceCounter(0)
+		, pin(0x04)
+		, debounceCounterThreshold(10)
+	{
+	}
+
+	inline bool isPressed(){
+		return true;//((lastState = state), (state = PIND & pin), state); 
+	}	
+
+	int debounce(){
+		if(isPressed() != lastState){
+			// Rising edge			
+			if(state){
+				debounceCounter = 0;
+			}else{ // Falling edge
+				btnReleased();	
+			}
+
+		}
+		
+		if(state){
+			debounceCounter++;
+			if(debounceCounter >= debounceCounterThreshold){
+				btnPressed();
+			}
+			PORTB = 0x01;
+		}
+		return 0;	
+	}	
+
+
+private:
+	const unsigned int pin;
+	const unsigned int debounceCounterThreshold;
+	unsigned int debounceCounter;
 	
+public:
+	int (*btnPressed)();
+	int (*btnReleased)();
+	bool state;
+	bool lastState;
+ 
+	
+};
+
+
+int bPressed(){
+	PORTB = 0x02; // TODO set proper bit
+	return 0;
+}
+int bReleased(){
+	PORTB - 0x00;
+	return 0;
+}
+
+int main(){
+	DDRB = 0xFF;
+	DDRD = 0x00;
+	PORTB= 0x01;
+
+	Btn btn = Btn();
+	btn.btnPressed = &bPressed;
+	btn.btnReleased = &bReleased;
+	
+	while(1){
+		btn.debounce();
+		_delay_ms(10);
+	}	
 
 	return 0;
 }
